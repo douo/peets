90ab79fefe1f482db1a025216d961c712401dbd9
tiou <dourokinga@gmail.com>
2022-11-29 17:53:45 +0800

    pytest: improve fixture: hijack

--- test/test_tmdb.py
+++ test/test_tmdb.py
@@ -15,92 +15,87 @@ from contextlib import contextmanager
 
 
 def test_detail(hijack):
-    with hijack() as mock:
-        data = mock("movie.json")
-        tmdb = TmdbMovieMetadata(language=Language.ZH, country=Country.CN)
-        m = tmdb.apply(Movie(), id_=0)
+    data = hijack("movie.json")
+    tmdb = TmdbMovieMetadata(language=Language.ZH, country=Country.CN)
+    m = tmdb.apply(Movie(), id_=0)
 
-        assert m.ids["imdb"] == data["imdb_id"]
-        assert int(m.ids["tmdb"]) == data["id"]
-        assert m.release_date == "2022-07-08"
-        assert m.certification == MediaCertification.US_PG13
-        assert m.movie_set.tmdb_id == 131296
-        assert m.ids["tmdbSet"] == "131296"
-        # print(generate_nfo(m))
+    assert m.ids["imdb"] == data["imdb_id"]
+    assert int(m.ids["tmdb"]) == data["id"]
+    assert m.release_date == "2022-07-08"
+    assert m.certification == MediaCertification.US_PG13
+    assert m.movie_set.tmdb_id == 131296
+    assert m.ids["tmdbSet"] == "131296"
+    # print(generate_nfo(m))
 
 
 def test_artwork(hijack):
-    with hijack() as mock:
-        mock("movie_images.json")
+    hijack("movie_images.json")
 
-        tmdb = TmdbArtworkProvider(language=Language.ZH, country=Country.CN)
+    tmdb = TmdbArtworkProvider(language=Language.ZH, country=Country.CN)
 
-        m = tmdb.apply(Movie(ids={"tmdb": 0}))
+    m = tmdb.apply(Movie(ids={"tmdb": 0}))
 
-        assert m.artwork_url_map == {
-            MediaFileType.POSTER: "https://image.tmdb.org/t/p/original/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-            MediaFileType.FANART: "https://image.tmdb.org/t/p/original/5pxdgKVEDWDQBtvqIB2eB2oheml.jpg",
-        }
+    assert m.artwork_url_map == {
+        MediaFileType.POSTER: "https://image.tmdb.org/t/p/original/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+        MediaFileType.FANART: "https://image.tmdb.org/t/p/original/5pxdgKVEDWDQBtvqIB2eB2oheml.jpg",
+    }
 
 
 def test_metadata_original_release_date_only(hijack):
-    with hijack() as mock:
-        mock("movie_original_release_date_only.json")
+    hijack("movie_original_release_date_only.json")
 
-        tmdb = TmdbMovieMetadata(language=Language.ZH, country=Country.CN)
-        m = tmdb.apply(Movie(), id_=0)
+    tmdb = TmdbMovieMetadata(language=Language.ZH, country=Country.CN)
+    m = tmdb.apply(Movie(), id_=0)
 
-        assert m.release_date == "2022-07-08"
+    assert m.release_date == "2022-07-08"
 
 
 def test_metadata_certification(hijack):
-    with hijack() as mock:
-        mock("movie_certification.json")
-        tmdb = TmdbMovieMetadata(language=Language.ZH, country=Country.CN)
-        m = tmdb.apply(Movie(), id_=0)
+    hijack("movie_certification.json")
+    tmdb = TmdbMovieMetadata(language=Language.ZH, country=Country.CN)
+    m = tmdb.apply(Movie(), id_=0)
 
-        assert m.certification == MediaCertification.US_PG13
+    assert m.certification == MediaCertification.US_PG13
 
 
 def test_tvshow(hijack):
-    with hijack() as mock:
-        mock("tvshow.json", "tmdbsimple.TV._GET")
-        mock("season.json", "tmdbsimple.TV_Seasons._GET")
-        tmdb = TmdbMovieMetadata(language=Language.ZH, country=Country.CN)
-        episodes = [TvShowEpisode(season=1, episode=1)]
-        m = tmdb.apply(TvShow(episodes=episodes), id_=0)
+    hijack("tvshow.json", "tmdbsimple.TV._GET")
+    hijack("season.json", "tmdbsimple.TV_Seasons._GET")
+    tmdb = TmdbMovieMetadata(language=Language.ZH, country=Country.CN)
+    episodes = [TvShowEpisode(season=1, episode=1)]
+    m = tmdb.apply(TvShow(episodes=episodes), id_=0)
 
-        assert m.ids["tmdb"] == "95396"
-        assert m.title == "Severance"
-        assert m.first_aired == "2022-02-17"
-        assert m.country == "US"
-        assert m.runtime == 50
-        assert m.production_company == "Apple TV+, Red Hour, Endeavor Content, Fifth Season"
-        assert m.status == MediaAiredStatus.CONTINUING
-        assert m.year == 2022
-        assert len(m.actors) == 10
-        assert m.ids["imdb"] == "tt11280740"
-        assert m.ids["tvdb"] == "371980"
-        assert m.certification == MediaCertification.US_TVMA
-        assert set(m.genres) == {
-            MediaGenres.DRAMA,
-            MediaGenres.SCIENCE_FICTION,
-            MediaGenres.MYSTERY,
-        }
-        assert len(m.seasons) == 1
-        s = m.seasons[0]
-        assert s.air_date == "2022-02-17"
-        assert s.episode_count == 9
-        assert s.name == "第 1 季"
-        assert s.season == 1
+    assert m.ids["tmdb"] == "95396"
+    assert m.title == "Severance"
+    assert m.first_aired == "2022-02-17"
+    assert m.country == "US"
+    assert m.runtime == 50
+    assert m.production_company == "Apple TV+, Red Hour, Endeavor Content, Fifth Season"
+    assert m.status == MediaAiredStatus.CONTINUING
+    assert m.year == 2022
+    assert len(m.actors) == 10
+    assert m.ids["imdb"] == "tt11280740"
+    assert m.ids["tvdb"] == "371980"
+    assert m.certification == MediaCertification.US_TVMA
+    assert set(m.genres) == {
+        MediaGenres.DRAMA,
+        MediaGenres.SCIENCE_FICTION,
+        MediaGenres.MYSTERY,
+    }
+    assert len(m.seasons) == 1
+    s = m.seasons[0]
+    assert s.air_date == "2022-02-17"
+    assert s.episode_count == 9
+    assert s.name == "第 1 季"
+    assert s.season == 1
 
-        assert len(m.episodes) == 1
-        e = m.episodes[0]
-        assert e.first_aired == "2022-02-17"
-        assert e.ids["tmdb"] == "1982925"
-        assert e.season == 1
-        assert e.episode == 1
-        assert len(e.actors) == 9
+    assert len(m.episodes) == 1
+    e = m.episodes[0]
+    assert e.first_aired == "2022-02-17"
+    assert e.ids["tmdb"] == "1982925"
+    assert e.season == 1
+    assert e.episode == 1
+    assert len(e.actors) == 9
 
 
 @fixture
@@ -108,45 +103,41 @@ def hijack(datadir, monkeypatch, request):
     '''
     包装了 monkeypatch fixture，方便用文件内容替换掉函数返回
     '''
-    @contextmanager
-    def _inner():
+    def parse(path):
+        '''
+        将 tmdbsimple.base.TMDB._GET 转换为 (tmdbsimple.base.TMDB, '_GET')（类引用，'方法名'）
+        '''
+        end = len(path)
+        while True:
+            try:
+                ridx = path.rindex(".", 0, end)
+            except ValueError as exp:
+                raise ModuleNotFoundError() from exp
+            if ridx != -1 and path[:ridx] in sys.modules:
+                mod = sys.modules[path[:ridx]]
+                break
+            end = ridx
 
-        def parse(path):
+        if mod:
+            sub = path[ridx + 1 :]
+            refs = sub.split(".")
+            pre = mod
+            for r in refs[:-1]:
+                pre = getattr(pre, r)
+            return (pre, refs[-1])
+
+    with monkeypatch.context() as m:
+        def mock(name: str, path="tmdbsimple.base.TMDB._GET"):
             '''
-            将 tmdbsimple.base.TMDB._GET 转换为 (tmdbsimple.base.TMDB, '_GET')（类引用，'方法名'）
+            将文件内容作为函数的返回
+
+            @name str: 文件名
+            @path str: 函数路径
             '''
-            end = len(path)
-            while True:
-                try:
-                    ridx = path.rindex(".", 0, end)
-                except ValueError as exp:
-                    raise ModuleNotFoundError() from exp
-                if ridx != -1 and path[:ridx] in sys.modules:
-                    mod = sys.modules[path[:ridx]]
-                    break
-                end = ridx
+            with open(f"{datadir}/{name}") as f:
+                data = json.load(f)
+            clazz, func = parse(path)
+            m.setattr(clazz, func, lambda *x: data)
+            return data
+        yield mock
 
-            if mod:
-                sub = path[ridx + 1 :]
-                refs = sub.split(".")
-                pre = mod
-                for r in refs[:-1]:
-                    pre = getattr(pre, r)
-                return (pre, refs[-1])
-
-        with monkeypatch.context() as m:
-            def mock(name: str, path="tmdbsimple.base.TMDB._GET"):
-                '''
-                将文件内容作为函数的返回
-
-                @name str: 文件名
-                @path str: 函数路径
-                '''
-                with open(f"{datadir}/{name}") as f:
-                    data = json.load(f)
-                clazz, func = parse(path)
-                m.setattr(clazz, func, lambda *x: data)
-                return data
-            yield mock
-
-    return _inner
